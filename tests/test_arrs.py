a7fca9c6e0efe3bf155ee93be44998d8e4cb2c90
vp <mr.r9ch@yandex.ru>
2023-08-21 12:39:24 +0300

    88%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,8 +3,11 @@ import pytest
 def test2_get():
     with pytest.raises(IndexError):
         assert arrs.get([], 0, "test")
+        assert arrs.get([1, 2, 3], -2, "test") == ["test"]
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -4) == [ 1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
